7d164d6fc90d802c81c28bfaf905e552fc1b53e3
Ariel Fonticiella <ariel@fonticiella.io>
2018-02-21 17:07:42 -0500

    added a random quote generator prior to game starting

--- js/canvas.js
+++ js/canvas.js
@@ -6,18 +6,22 @@ let height = canvas.height= window.innerHeight;
 
 
 const quotes = [
-  "If you don't have anything nice to say, say something clever but devastating.",
-  "*Developer visibly offended you skipped his intro*"
+  "'If you don't have anything nice to say, say something clever but devastating.'",
+  "*Developer visibly offended you skipped his intro*",
+  "'It's easy to sit there and say you'd like to have more money. And I guess that's what I like about it.'",
+  "'It takes a big man to cry, but it takes a bigger man to laugh at that man.'",
+  "'How come the dove gets to be the peace symbol? How about the pillow? It has more feathers than the dove, and it doesn't have that dangerous beak'"
 ];
 
 $("#firstFade").append(function random(){
-  let result = Math.floor(Math.random() * 3);
+  let max = quotes.length;
+  let result = Math.floor(Math.random() * max);
   let list = quotes.map((quote) => {
   return quote;
   });
   return list[result];
 }());
-$("#firstFade").show(0).delay(5000).hide(2);
+$("#firstFade").show(0).delay(4400).hide(5);
 
 
 
