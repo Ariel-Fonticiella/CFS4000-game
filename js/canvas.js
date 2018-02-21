d93e54f18c72bd3c568231d317be7e8b6f8261a0
Ariel Fonticiella <ariel@fonticiella.io>
2018-02-21 16:48:45 -0500

    added a random quote generator prior to game starting

--- js/canvas.js
+++ js/canvas.js
@@ -1,8 +1,25 @@
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
 
-var width = canvas.width = window.innerWidth;
-var height = canvas.height= window.innerHeight;
+let width = canvas.width = window.innerWidth;
+let height = canvas.height= window.innerHeight;
+
+
+const quotes = [
+  "If you don't have anything nice to say, say something clever but devastating.",
+  "*Developer visibly offended you skipped his intro*"
+];
+
+$("#firstFade").append(function random(){
+  let result = Math.floor(Math.random() * 3);
+  let list = quotes.map((quote) => {
+  return quote;
+  });
+  return list[result];
+}());
+$("#firstFade").show(0).delay(5000).hide(2);
+
+
 
 // var img = new Image();
 //
